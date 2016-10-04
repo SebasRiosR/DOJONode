d5385614b6e2763abe11c05605315c0e12dada74
SebasRiosR <sebastian.riosr@udea.edu.co>
2016-10-04 13:04:01 -0500

    Los 3 ejercicios del DOJO de NodeJS

---
+++ DOJONode ejercicio 2 con menu/routes.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var db = require("./queries");
+
+router.get('/api/restaurants', db.getAllRestaurants);
+router.get('/api/restaurants/:name', db.getRestaurantByName);
+router.post('/api/restaurants', db.createRestaurant);
+router.delete('/api/restaurants/:id', db.removeRestaurant);
+router.put('/api/restaurants/:id', db.updateRestaurant);
+router.get('/api/menu', db.getAllMenus);
+router.get('/api/menu/:id', db.getMenuById);
+router.post('/api/menu', db.createMenu);
+router.delete('/api/menu/:id', db.removeMenu);
+router.put('/api/menu/:id', db.updateMenu);
+router.get('/api/restaurants/menu/:id' , db.menuPorRestaurante);
+
+module.exports = router;
